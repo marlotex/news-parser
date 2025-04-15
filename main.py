86fe52e309b891762559b4731eff4ef4f90aa008
Lowflyscoot <89293466+Lowflyscoot@users.noreply.github.com>
2025-04-15 12:37:56 +0400

    Исправлено открытие порта

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from playwright.sync_api import sync_playwright
+import os
 
 app = Flask(__name__)
 
@@ -24,4 +25,5 @@ def scrape():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=5000)
